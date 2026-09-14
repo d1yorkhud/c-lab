ef8e5fc874630c0fe8f77e634d1fe1cbeb9ffc50
d1yorkhud <d1yorkhud@gmail.com>
2026-09-14 17:10:23 +0900

    chap 1.2 fahr

---
+++ kernighan-ritchie/ch1-2-fahr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+ for fahr = 0, 20, ..., 300; floating-point version */
+
+ int main() {
+    float fahr, celcius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr<=upper)
+    {
+        celcius = (5.0/9.0) * (fahr-32.0);
+        // %f says to print the number as floating point
+        printf("%3.0f %6.1f\n", fahr, celcius);
+        fahr = fahr + step;
+    }
+    
+    
+ }
+
+
+ /*
+ %d  ->  print as decimal integer
+ %6d  ->  print as decimal integer, at least 6 characters wide
+ %f ->  print as floating point
+ %6f ->  print as floating point, at least 6 characters wide
+ %.2f ->  print as floating point, 2 characters after decimal point
+ %6.2f ->  print as floating point, at least 6 wide and 2 after decimal point 
+ */
