df30150305afd757ee7ba182b446e0a645a826ed
d1yorkhud <d1yorkhud@gmail.com>
2026-09-15 17:11:56 +0900

    exercise 1.5

---
+++ kernighan-ritchie/chapter-1/exerc1-5.c
@@ -0,0 +1,18 @@
+/*
+Modify the temperature conversion program to print the table in reverse order,
+that is, from 300 degrees to 0.
+*/
+
+#include <stdio.h>
+
+int main(){
+    int fahr;
+
+    printf("Fahr to Celsius in reverse order\n\n");
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+
+    }
+    return 0;
+}
